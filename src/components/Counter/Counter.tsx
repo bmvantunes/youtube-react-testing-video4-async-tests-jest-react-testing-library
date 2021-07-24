e78c3369e15d7fbbe7ce61011f8971ce3bb7313f
unknown <bmvantunes@outlook.com>
2021-07-24 11:09:37 +0100

    forked from video3

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+export interface CounterProps {
+  description: string;
+  defaultCount: number;
+}
+
+export function Counter({ description, defaultCount }: CounterProps) {
+  const [count, setCount] = useState(defaultCount);
+  const [incrementor, setIncrementor] = useState(1);
+
+  return (
+    <div>
+      <h2>
+        DESC: {description} - DC: {defaultCount}
+      </h2>
+      <label>
+        Incrementor:
+        <input
+          value={incrementor}
+          onChange={(evt) => {
+            setIncrementor(parseInt(evt.target.value) || 1);
+          }}
+          type="number"
+        />
+      </label>
+      <button
+        aria-label="Subtract from Counter"
+        onClick={() => setCount(count - incrementor)}
+      >
+        -
+      </button>
+      Current Count: {count}
+      <button
+        aria-label="Add to Counter"
+        onClick={() => setCount(count + incrementor)}
+      >
+        +
+      </button>
+    </div>
+  );
+}
